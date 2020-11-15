b9e5d4499f8c72371e0406e032d7a46c1451b9c9
yangtao-code <74482266+yangtao-code@users.noreply.github.com>
2020-11-15 19:44:54 +0800

    Add files via upload

---
+++ 乘法.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+
+int main()
+{
+	int num1 = 0, num2 = 0, sum = 0;
+	scanf_s("%d%d", &num1, &num2);
+	sum = num1 + num2;
+	printf("sum=%d\n", sum);
+		return 0;
+}
---
+++ 九九乘法表.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int a, b;
+	for (a = 1; a <= 9; a++)
+	{
+		for (b = 1; b <= a; b++)
+			printf("%d*%d=%2d",a, b, a*b  );
+		printf("\n");
+	}
+	return 0;
+}
